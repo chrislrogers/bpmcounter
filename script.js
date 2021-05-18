51871f7f92efd5dc279f7c02620bba1279020395
Chris Rogers <47615352+chrislrogers@users.noreply.github.com>
2021-05-18 18:41:56 +0100

    code formatting

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const bpmHTML = document.getElementById("bpm");
 const instHTML = document.getElementById("instructions");
 
 let counter = 0; // number of times user has tapped.
-let startTime  = 0; // time user started tapping.
+let startTime = 0; // time user started tapping.
 
-document.addEventListener("click", function(){
+document.addEventListener("click", function () {
     let date = new Date();
     let time = date.getTime(); // gets time in milliseconds
-    
+
     if (counter === -1) {
         counter = 0;
     } else if (counter === 0) {
@@ -22,7 +22,7 @@ document.addEventListener("click", function(){
 
 function reset() {
     counter = -1;
-    startTime  = 0;
+    startTime = 0;
     instHTML.innerHTML = "Tap or click to begin.";
     bpmHTML.innerHTML = "0";
 }
