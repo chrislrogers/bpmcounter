c615bdd8becc45d18937b9ab5f16c4d31aabf012
Chris Rogers <47615352+chrislrogers@users.noreply.github.com>
2021-05-18 15:53:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const bpmHTML = document.getElementById("bpm");
+const instHTML = document.getElementById("instructions");
+
+let counter = 0; // number of times user has tapped.
+let startTime  = 0; // time user started tapping.
+
+document.addEventListener("click", function(){
+    let date = new Date();
+    let time = date.getTime(); // gets time in milliseconds
+    if (counter === 0){
+        instHTML.innerHTML = "Tap again.";
+        startTime = time;
+        counter++;
+    } else {
+        bpmHTML.innerHTML = Math.round(60000 * counter / (time - startTime));
+        counter++;
+    }
+});
+
+function reset() {
+    counter    = -1;
+    startTime  = 0;
+    instHTML.innerHTML = "Tap or click to begin.";
+}
